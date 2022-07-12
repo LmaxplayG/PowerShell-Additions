469daa94a2a8c0a2f355a9525e37ced3494f8a24
LmaxplayG <lucamaxplay@gmail.com>
2022-07-12 15:19:31 +0200

    Fixed code in easybuild.ps1

--- Scripts/easybuild.ps1
+++ Scripts/easybuild.ps1
@@ -7,17 +7,40 @@ Builds the current project (using Make or a build.ps1 file)
 System.Null
 #>
 function Build-Project () {
-    if ( Test-Path -Path "./build.ps1" -ErrorAction SilentlyContinue ) {
+    if ( Test-Path -Path "./build.ps1" -ErrorAction SilentlyContinue )
+    {
         ./build.ps1
-    } elseif ( Test-Path -Path "./makefile" -ErrorAction SilentlyContinue ) {
+    }
+    elseif ( Test-Path -Path "./makefile" -ErrorAction SilentlyContinue ) {
+        if (Get-Command "make" -ErrorAction SilentlyContinue) {
         make
-    } elseif ( Test-Path -Path "./gradlew" -ErrorAction SilentlyContinue ) {
+        } else {
+            Write-Output -NoNewline -ForegroundColor: "Red" "MVN wasn't found on path"
+        }
+    }
+    elseif ( Test-Path -Path "./gradlew" -ErrorAction SilentlyContinue ) {
+
         gradlew build
-    } elseif ( Test-Path -Path "./pom.xml" -ErrorAction SilentlyContinue ) {
-        mvn "clean"
-    } elseif ( Test-Path -Path "./*.csproj" -ErrorAction SilentlyContinue ) {
+
+    }
+    elseif ( Test-Path -Path "./pom.xml" -ErrorAction SilentlyContinue )
+    {
+        if (Get-Command "mvn" -ErrorAction SilentlyContinue) {
+            mvn "clean"
+        } else {
+            Write-Output -NoNewline -ForegroundColor: "Red" "MVN wasn't found on path"
+        }
+    }
+    elseif ( Test-Path -Path "./*.csproj" -ErrorAction SilentlyContinue )
+    {
+        if (Get-Command "dotnet" -ErrorAction SilentlyContinue) {
         dotnet build
-    } else {
+        } else {
+            Write-Output -NoNewline -ForegroundColor: "Red" "DOTNET wasn't found on path"
+        }
+    }
+    else
+    {
         Write-Host -NoNewline -ForegroundColor "Red" "Unable to find a project to build`n"
     }
 }
