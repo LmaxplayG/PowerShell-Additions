ce0352af7637435f6409571b80fd1d5931479586
LmaxplayG <lucamaxplay@gmail.com>
2022-07-13 15:28:28 +0200

    Git.ps1 formatting

--- Scripts/git.ps1
+++ Scripts/git.ps1
@@ -6,20 +6,22 @@ function Get-GitBranch {
             $path
             $path = Split-Path -Path $path -Parent        
         } while ($path)
-    } |% {
+    } | % {
         $headPath = "$_/.git/HEAD"
         if (Test-Path $headPath) {
             $ref = get-content $headPath -TotalCount 1        
             if ($ref.StartsWith("ref:")) {
                 $ref.Substring(5).Split("/") | select -Skip 2 | Join-String -Separator "/"
-            } else {
+            }
+            else {
                 $ref.Substring(0, 8)
             }
         }
     } | Select -First 1
-    if($output -eq "") {
+    if ($output -eq "") {
         return $null
-    } else {
+    }
+    else {
         return $output
     }
 }
